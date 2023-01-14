e6d53ed6bde61d685272f0c864c764d9aed64a07
Пользователь <flamepw11@yandex.ru>
2023-01-14 17:31:50 +0300

    s

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from users.views import UserViewSet
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     ReviewViewSet, TitleViewSet)
-from users.views import UserViewSet
 
 app_name = 'api'
 
